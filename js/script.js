3b75b7dedff95f18e01cdd96f0b6dd2d2ea512a1
Charles <charlesbcookman@gmail.com>
2023-01-25 10:36:08 -0800

    testing currentTurn array to display current turns values
    Co-Authored-By Zookerman3 <szook7@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 
+function currentTurn(array){
+    const p = document.createElement("p");
+    const displayDiv = document.querySelector("#current-turn-values");
+    array.forEach(element => {
+        p.append("Rolled: " + element);
 
+    });
+}
 
 function roll(){
     return Math.floor(Math.random() * 6 + 1)
 }
 
-window.addEventListener
+window.onload = function(){
+    let turnValues = [];
+    const rollButton = document.querySelector("#roll");
+    const endTurnButton = document.querySelector("#end-turn");
+    const resetButton = document.querySelector("#reset");
+    rollButton.onclick = function() {
+        currentTurn(turnValues.push.roll());
+    }
+};
