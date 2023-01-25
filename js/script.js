2a097c1bde120151d0a3b2b70f1beb23d1f5b4eb
Charles <charlesbcookman@gmail.com>
2023-01-25 11:39:50 -0800

    players current dice rolls will display

--- js/script.js
+++ js/script.js
@@ -1,23 +1,43 @@
 
-function currentTurn(array){
+function currentTurn(turn){
     const p = document.createElement("p");
     const displayDiv = document.querySelector("#current-turn-values");
-    array.forEach(element => {
-        p.append("Rolled: " + element);
-
+    let turnString = ""
+    console.log(turn);
+    turn.forEach(element => {
+    turnString = ("Rolled: " + element);
     });
+    p.innerText = turnString;
+    displayDiv.append(p);
 }
 
 function roll(){
     return Math.floor(Math.random() * 6 + 1)
 }
 
+function endTurn(values, div){
+    // values.forEach()
+    div.innerHTML = '';
+}
+
 window.onload = function(){
     let turnValues = [];
+    let p1Score = 0;
+    let p2Score = 0;
     const rollButton = document.querySelector("#roll");
     const endTurnButton = document.querySelector("#end-turn");
     const resetButton = document.querySelector("#reset");
-    rollButton.onclick = function() {
-        currentTurn(turnValues.push.roll());
+    const p = document.createElement("p");
+    const displayDiv = document.querySelector("#current-turn-values");
+
+    rollButton.onclick = function(){
+    turnValues.push(roll());
+    currentTurn(turnValues);
     }
-};
+
+    endTurnButton.onclick = function(){
+        endTurn(turnValues, displayDiv, p1Score, p2Score);
+        turnValues = [];
+    }
+}
+
