7c91e444e881cf9832953ddccefe933d669b10a1
Charles <charlesbcookman@gmail.com>
2023-01-25 13:59:44 -0800

    scores are switching players as intended
    Co-Authored-By Zookerman3 <szook7@gmail.com>

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ function currentTurn(turn){
     const p = document.createElement("p");
     const displayDiv = document.querySelector("#current-turn-values");
     let turnString = ""
-    console.log(turn);
     turn.forEach(element => {
     turnString = ("Rolled: " + element);
     });
     p.innerText = turnString;
     displayDiv.append(p);
+    return turn
 }
 
 function roll(){
@@ -16,18 +16,34 @@ function roll(){
 }
 
 function endTurn(values, div){
-   
     div.innerHTML = '';
 }
 
+
 function addToTotal(numbers){
+    let p1Turn = document.querySelector("#p1turn");
+    let p2Turn = document.querySelector("#p2turn");
+    let p1Score = document.querySelector("#p1score");
+    let p2Score = document.querySelector("#p2score");
     let playerSum = 0
-    
+    const p1id = 1
+    const p2id = 2
     numbers.forEach(number => {
         playerSum += number;
     });
-    
-    
+    if (p2Turn.classList.value === 'hidden') {
+        p1Score.innerText = playerSum.toString();
+        p2Turn.removeAttribute("class", 'hidden');
+        p1Turn.setAttribute("class", 'hidden');
+    } else if (p1Turn.classList.value === 'hidden'){
+        p2Score.innerText = playerSum.toString();
+        p2Turn.setAttribute("class", 'hidden');
+        p1Turn.removeAttribute("class", 'hidden');
+    } else {
+        console.log("if is broken");
+    }
+    console.log(playerSum)
+
 }
 
 
@@ -48,12 +64,13 @@ window.onload = function(){
     rollButton.onclick = function(){
     turnValues.push(roll());
     currentTurn(turnValues);
-   
+    
     }
-    console.log(turnValues) 
-    addToTotal(turnValues)
+    
 
     endTurnButton.onclick = function(){
+        console.log(currentTurn(turnValues)) 
+        addToTotal(currentTurn(turnValues))
         endTurn(turnValues, displayDiv);
         turnValues = [];
     }
