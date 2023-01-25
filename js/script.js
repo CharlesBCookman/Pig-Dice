71228710a3a4ceaf48ecf432bac6ddc56f3f0f73
Charles <charlesbcookman@gmail.com>
2023-01-25 14:36:49 -0800

    finished functionality for displaying winner and final totals
    Co-Authored-By Zookerman3 <szook7@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+//business logic
 function currentTurn(turn){
     const p = document.createElement("p");
     const displayDiv = document.querySelector("#current-turn-values");
@@ -20,35 +20,50 @@ function endTurn(values, div){
 }
 
 
+
 function addToTotal(numbers){
+
+    let playerSum = 0
+    if (numbers.includes(1)){
+    } else {
+    numbers.forEach(number => {
+        playerSum += number;
+        });
+    }
+    checkAndSet(playerSum);
+    console.log(playerSum);
+
+}
+
+//UI Logic
+
+function checkAndSet(sum) {
     let p1Turn = document.querySelector("#p1turn");
     let p2Turn = document.querySelector("#p2turn");
     let p1Score = document.querySelector("#p1score");
     let p2Score = document.querySelector("#p2score");
-    let playerSum = 0
-    const p1id = 1
-    const p2id = 2
-    numbers.forEach(number => {
-        playerSum += number;
-    });
     if (p2Turn.classList.value === 'hidden') {
-        p1Score.innerText = playerSum.toString();
+        p1Score.innerText = (parseInt(p1Score.innerText) + sum).toString();
         p2Turn.removeAttribute("class", 'hidden');
         p1Turn.setAttribute("class", 'hidden');
+        checkWinner(parseInt(p1Score.innerText), p1Score);
     } else if (p1Turn.classList.value === 'hidden'){
-        p2Score.innerText = playerSum.toString();
+        p2Score.innerText = (parseInt(p2Score.innerText) + sum).toString();
         p2Turn.setAttribute("class", 'hidden');
         p1Turn.removeAttribute("class", 'hidden');
+        checkWinner(parseInt(p2Score.innerText), p2Score);
     } else {
         console.log("if is broken");
     }
-    console.log(playerSum)
-
 }
 
-
-
-
+function checkWinner(number, element) {
+    if (number >= 40) {
+        element.innerText = "WINNER WINNER CHICKEN DINNER!!!!";
+    } else {
+        //nothing
+    }
+}
 
 
 window.onload = function(){
